b00a433c585b4207b2e06ad5b946b0d5d2a15d52
Thomas Biesaart <thomas@infi.nl>
2020-10-03 16:55:42 +0200

    Set up basics for create server page

--- admin-gs-webui/src/pages/gameservers/NewGameServer.tsx
+++ admin-gs-webui/src/pages/gameservers/NewGameServer.tsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { useParams } from 'react-router-dom';
 import {Layout} from "../../components";
 
-export const NewGameServer: React.FC = () => (
-    <Layout name="new-game-server">
-        <h1>New Server</h1>
-    </Layout>
-);
+export const NewGameServer: React.FC = () => {
+    const {nodeId} = useParams<{nodeId: string}>();
+    const [games, setGames] = useState<any[]>([]);
+    const [nodes, setNodes] = useState<any[]>([]);
+    const [node, setNode] = useState<string>(nodeId);
+    const [game, setGame] = useState<string>();
+
+    useEffect(() => {
+        fetch('/api/games')
+            .then(r => r.json())
+            .then(setGames)
+    }, []);
+
+    useEffect(() => {
+        fetch('/api/nodes')
+            .then(r => r.json())
+            .then(setNodes)
+    }, []);
+
+    return (
+        <Layout name="new-game-server">
+            <h1>New Server</h1>
+
+            <label htmlFor="node">Node</label>
+            <select id="node" name="node" value={node} onChange={e => setNode(e.target.value)}>
+                <option>Select a node...</option>
+                {nodes.map(node => (
+                    <option value={node.id}>{node.name}</option>
+                ))}
+            </select>
+
+            <label htmlFor="game">Game</label>
+            <select id="game" name="game" value={game} onChange={e => setGame(e.target.value)}>
+                <option>Select a game...</option>
+                {games.map(game => (
+                    <option value={game.id}>{game.name}</option>
+                ))}
+            </select>
+        </Layout>
+    );
+};
